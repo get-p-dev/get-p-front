8f5eae97f03c44da519ed12a9804bbe58e082f92
Chaejun Lee <chaejunlee@knu.ac.kr>
2022-04-27 20:36:00 +0900

    Updated Promotion Section

--- src/PersonPromotion.js
+++ src/PersonPromotion.js
@@ -5,7 +5,7 @@ function PersonPromotion() {
     const PromotionList = [
         {
             data: 1,
-            imageUrl: "https://picsum.photos/id/10/500/600"
+            imageUrl: "https://picsum.photos/id/10/500/500"
         },
         {
             data: 2,
@@ -21,24 +21,38 @@ function PersonPromotion() {
         }
     ]
 
+    function imageCover(url) {
+        return {
+            backgroundImage: `url(${url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }
+    }
+
     return (
         <div className="max-w-6xl mx-auto px-4 mt-12">
-            <h2 className="text-4xl font-bold mx-4">Recommended People</h2>
+            <div className="flex w-full justify-end pt-4 px-2">
+                <h2 className="grow text-4xl font-bold">Recommended People</h2>
+                <a href="#top" className="grow-0 bg-gray-100 px-4 py-2 rounded-md text-lg hover:bg-gray-300 transition duration-250 ease-in-out">+ more</a>
+            </div>
             <div className="grid grid-cols-4 grid-rows-1 gap-4 mt-8 h-60">
                 {
                     PromotionList.map((element, index) => {
                         return (
                             <a href="#top" key={index}>
-                                <div className="flex items-center justify-center rounded-md h-full bg-gray-300 hover:scale-105 transition ease-in-out overflow-hidden">
-                                    <img className="bg-cover" src={element.imageUrl} alt={element.data}></img>
+                                <div className="relative flex items-center justify-center rounded-md h-full w-full hover:scale-105 transition ease-in-out overflow-hidden">
+                                    <div className="w-full h-full bg-cover" style={imageCover(element.imageUrl)}>
+                                        <div className="absolute bg-gradient-to-t from-gray-200/80 to-transparent w-full h-full">
+                                            <div className="flex h-full px-4 pb-2 items-end">
+                                                <p className="font-bold text-lg">Steve Steveson</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </a>
                         )
                     })
                 }
-            </div>
-            <div className="flex w-full justify-end pt-4">
-                <a href="#top" className="bg-gray-100 px-4 py-2 rounded-md text-lg hover:bg-gray-300 transition duration-250 ease-in-out">+ more</a>
             </div>
         </div>
     )
--- src/ProjectPromotion.js
+++ src/ProjectPromotion.js
@@ -5,65 +5,71 @@ function ProjectPromotion() {
         {
             featured: 1,
             data: 1,
-            imageUrl: "https://picsum.photos/id/1/500/600"
+            imageUrl: "https://picsum.photos/id/1/800/450"
         },
         {
             featured: 0,
             data: 2,
-            imageUrl: "https://picsum.photos/id/2/500/500"
+            imageUrl: "https://picsum.photos/id/2/800/450"
         },
         {
             featured: 0,
             data: 3,
-            imageUrl: "https://picsum.photos/id/3/500/500"
+            imageUrl: "https://picsum.photos/id/3/800/450"
         },
         {
             featured: 0,
             data: 4,
-            imageUrl: "https://picsum.photos/id/4/500/500"
+            imageUrl: "https://picsum.photos/id/4/800/450"
         },
         {
             featured: 0,
             data: 5,
-            imageUrl: "https://picsum.photos/id/5/500/500"
+            imageUrl: "https://picsum.photos/id/5/800/450"
         },
-        {
-            featured: 0,
-            data: 6,
-            imageUrl: "https://picsum.photos/id/6/500/500"
-        },
-        {
-            featured: 0,
-            data: 7,
-            imageUrl: "https://picsum.photos/id/7/500/500"
-        }
     ]
+
+    function imageCover(url) {
+        return {
+            backgroundImage: `url(${url})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }
+    }
 
     return (
         <div className="max-w-6xl mx-auto px-4 mt-12">
-            <h2 className="font-sans text-4xl font-bold mx-4">Trending Projects</h2>
-            <div className="grid grid-cols-4 grid-rows-2 gap-4 mt-8 h-72">
+            <div className="flex w-full pt-4 px-2">
+                <h2 className="grow text-4xl font-bold">Trending Projects</h2>
+                <a href="#top" className="grow-0 bg-gray-100 px-4 py-2 rounded-md text-lg hover:bg-gray-300 transition duration-250 ease-in-out">+ more</a>
+            </div>
+            <div className="grid grid-cols-4 grid-rows-2 gap-4 mt-8 h-96">
                 {
                     PromotionList.map((element, index) => {
                         return (
                             (element.featured === 1) ?
-                                <a href="#top" key={index} className="flex object-fill items-center justify-center row-span-2 rounded-md bg-gray-300 hover:scale-105 transition ease-in-out overflow-hidden">
-                                    <div>
-                                        <img className="bg-cover" src={element.imageUrl} alt={element.data}></img>
+                                <a href="#top" key={index} className="relative flex object-fill items-center justify-center row-span-2 col-span-2 rounded-md bg-gray-300 hover:scale-105 transition ease-in-out overflow-hidden">
+                                    <div className="h-full w-full" style={imageCover(element.imageUrl)}>
+                                        <div className="absolute bg-gradient-to-t from-gray-200/80 to-transparent w-full h-full">
+                                            <div className="flex h-full px-4 pb-4 items-end">
+                                                <p className="font-bold text-3xl">Steve Steveson</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </a>
                                 :
-                                <a href="#top" key={index} className="flex items-center justify-center rounded-md h-full bg-gray-300 hover:scale-105 transition ease-in-out overflow-hidden">
-                                    <div>
-                                        <img className="bg-cover" src={element.imageUrl} alt={element.data}></img>
+                                <a href="#top" key={index} className="relative flex items-center justify-center rounded-md h-full bg-gray-300 hover:scale-105 transition ease-in-out overflow-hidden">
+                                    <div className="h-full w-full" style={imageCover(element.imageUrl)}>
+                                        <div className="absolute bg-gradient-to-t from-gray-300/80 to-transparent w-full h-full">
+                                            <div className="flex h-full px-4 pb-2 items-end">
+                                                <p className="font-bold text-lg">Steve Steveson</p>
+                                            </div>
+                                        </div>
                                     </div>
                                 </a>
                         )
                     })
                 }
-            </div>
-            <div className="flex w-full justify-end pt-4">
-                <a href="#top" className="bg-gray-100 px-4 py-2 rounded-md text-lg hover:bg-gray-300 transition duration-250 ease-in-out">+ more</a>
             </div>
         </div >
     )
